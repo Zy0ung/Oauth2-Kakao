d192dcc28a689ac0d7429c71360626c96f1cb242
송지영 <jiy0ung_e@sch.ac.kr>
2024-10-28 14:37:36 +0900

    init

---
+++ src/main/java/com/example/oauth2_kakao/Oauth2KakaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_kakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2KakaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2KakaoApplication.class, args);
+	}
+
+}
